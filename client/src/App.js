d6a0a9dfcf34251d58214f18f3eda135767b4d97
King Elisha <ek.chibueze@gmail.com>
2019-11-14 16:57:37 +0100

    chat-space styles

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ const socket = openSocket("http://192.168.43.45:8000");
 
 export default class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    messageText: ''
   };
 
   componentDidMount() {
@@ -16,11 +17,38 @@ export default class App extends Component {
     });
   }
 
+  onChange = (e) => {
+    this.setState({
+      messageText: e.target.value
+    });
+  };
+
+  sendMessage = (e) => {
+    socket.emit('chat-msg', this.state.messageText);
+
+    this.setState({ messageText: '' });
+  };
+
   render() {
+    const { messageText } = this.state;
+
     return (
       <div className="app">
-        <h1>Count</h1>
-        <p>{this.state.count}</p>
+        {/* <h1>Count</h1>
+        <p>{this.state.count}</p> */}
+
+        <div className="chat-container">
+          <div className="chat-space">
+
+          </div>
+
+          <div className="chat-input">
+            <form id="form" onSubmit={this.sendMessage}>
+              <input type="text" placeholder="Type message" value={messageText} onChange={this.onChange} />
+              <input type="submit" value="Send" />
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
