2bc3904cfeaa1d7ee4ed0d9001375bbf896fd254
King Elisha <ek.chibueze@gmail.com>
2019-11-14 16:37:54 +0100

    connect

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,21 @@ import openSocket from 'socket.io-client';
 const socket = openSocket("http://192.168.43.45:8000");
 
 export default class App extends Component {
+  state = {
+    count: 0
+  };
+
+  componentDidMount() {
+    socket.on("count", (count) => {
+      this.setState({ count });
+    });
+  }
+
   render() {
     return (
       <div className="app">
-        <h1>Hello</h1>
+        <h1>Count</h1>
+        <p>{this.state.count}</p>
       </div>
     )
   }
